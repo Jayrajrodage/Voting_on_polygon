df011aa307bacbd027e4ea9fc5bc96597e46f00b
Yuvraj Rodage <jayrajrodge2001@gmail.com>
2023-04-12 12:12:52 +0530

    First Commite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { createContext, useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import Header from './Component/Header';
+import dao from "./Contract/Dao.json"
+const AppState = createContext();
 function App() {
+  const { ethereum } = window;
+  const [DaoContract, setDaoContract] = useState();
+  useEffect(() => {
+    const loadEthereum = async () => {
+      if (typeof window.ethereum !== 'undefined') {
+        const DaoContractAddress = "0x775C4c31AbBeaf170257805231E3d37D10908232"
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const DaoContract = new ethers.Contract(DaoContractAddress, dao.output.abi, signer);
+        setDaoContract(DaoContract)
+      } else {
+        console.log('No Ethereum provider found. Install Metamask.');
+      }
+    };
+    loadEthereum();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppState.Provider value={{
+      DaoContract
+    }}>
+      <div>
+        {typeof window.ethereum != "undefined" ?
+          <Header /> :
+          <div className='flex flex-col justify-center items-center mt-10'>
+            {/* install Metamask */}
+            <h1 class=" text-xl title-font  text-black mb-1">Install Metamask to access DAO</h1>
+            <a target={"_blank"} href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn">
+              <div className='flex border-opacity-60 bg-opacity-90 text-lg font-medium border-2 border-black cursor-pointer bg-black text-white mt-4 rounded-lg justify-center items-center py-1 px-2'>
+                Install Metamask
+                <img className='h-10' src='metamask.png' />
+              </div>
+            </a>
+            <p className='text-white text-lg mt-2'>Login Required Metamask Extension</p>
+          </div>
+        }
+      </div>
+    </AppState.Provider>
   );
 }
 
 export default App;
+export { AppState }
